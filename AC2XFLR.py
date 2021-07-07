e6b7e1f1e5705cb65011a29ba7333346efd0ce88
EliotDucky <EliotDucky@users.noreply.github.com>
2021-07-07 09:43:14 +0100

    Update AC2XFLR.py

--- AC2XFLR.py
+++ AC2XFLR.py
@@ -91,7 +91,6 @@ class Wing:
 		self.shape = shape_args["shape"]
 		if(self.shape == "ellipse"):
 			self.fsmf = shape_args["fsmf"]
-			global chordElliptical
 			self.chord_func = chordElliptical
 		self._type = _type
 		self.symmetric_fin = symmetric_fin
